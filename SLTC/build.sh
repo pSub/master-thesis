552afa0ab8de73476420cca4ef9812d00f291b98
Pascal Wittmann <mail@pascal-wittmann.de>
2014-08-23 14:32:14 +0200

    Add command to build template generation but somehow vampire is not called at all

--- SLTC/build.sh
+++ SLTC/build.sh
@@ -2,3 +2,12 @@
 
 # Build tests
 strc -i trans/sltc.str -m tests -la stratego-lib -la stratego-gpp -I . -I lib -o bin/tests
+
+# Generate a valid tbl
+sdf2table -i include/SLTC.def -m SLTC
+
+# Build template generator
+strc -i trans/sltc.str -m wrapped-generate-templates -la stratego-lib -la stratego-gpp -I . -I lib -o bin/.make-templates
+
+echo "sglri -p include/SLTC.tbl -i syntax/specifications/Subtyping-algorithmic/Typesystem.slt | bin/.make-templates" > bin/make-templates
+chmod +x bin/make-templates
