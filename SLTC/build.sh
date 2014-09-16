97924293b225aef34930478681302989eb3c4607
Pascal Wittmann <mail@pascal-wittmann.de>
2014-09-16 20:06:36 +0200

    Cleaned up build script

--- SLTC/build.sh
+++ SLTC/build.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
 
-FLAGS="-la stratego-lib -la stratego-gpp -la stratego-sdf -I . -I lib"
+FLAGS="-la stratego-lib"
+FLAGS="$FLAGS -la stratego-gpp"
+FLAGS="$FLAGS -la stratego-sdf"
+FLAGS="$FLAGS -I ."
+FLAGS="$FLAGS -I lib"
 
 # Build tests
-strc -i trans/sltc.str -m tests -la stratego-lib -la stratego-gpp -I . -I lib -o bin/tests
-
-# Generate a valid tbl
-sdf2table -i include/SLTC.def -m SLTC
+strc -i trans/sltc.str -m tests $FLAGS -o bin/tests
 
 # Build template generator
-strc -i trans/sltc.str -m wrapped-generate-templates $LIB -o bin/.make-templates
-
-echo "sglri -p include/SLTC.tbl -i $1 | bin/.make-templates" > bin/make-templates
-chmod +x bin/make-templates
-
-# Generate def for input terms
-pack-sdf -i syntax/Input.sdf -o include/Input.def
-
-# Generate tbl for input terms
-sdf2table -i include/Input.def -m Input
+strc -i trans/sltc.str -m wrapped-generate-templates $FLAGS -o bin/make-templates
 
 # Build a type checker
-strc -i trans/sltc.str -m wrapped-typecheck $FLAGS -o bin/.typecheck
-echo 'sglri -p include/SLTC.tbl -i $1 | bin/.typecheck -t $2' > bin/typecheck
-chmod +x bin/typecheck
+strc -i trans/sltc.str -m wrapped-typecheck $FLAGS -o bin/typecheck
+
